245706991c22cbc58868a72a07729d2dfa693e0c
Ryota Mine <ryotamine@gmail.com>
2019-03-20 22:30:42 +0000

    Fetch tweets with AJAX

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,54 +4,6 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-// User database
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": {
-        "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-        "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-        "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-      },
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": {
-        "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
-        "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
-        "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
-      },
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  },
-  {
-    "user": {
-      "name": "Johann von Goethe",
-      "avatars": {
-        "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
-        "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
-        "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
-      },
-      "handle": "@johann49"
-    },
-    "content": {
-      "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
-    },
-    "created_at": 1461113796368
-  }
-];
-
 // Render tweet function
 function renderTweets(tweets) {
   for (tweet of tweets) {
@@ -91,12 +43,27 @@ function createTweetElement(tweet) {
 
 $(document).ready(function() {
 
-  // POST tweets
-  $.post("/tweets", $(this).serialize(), function(event) {
-    console.log("It works!");
-  });
+  // Load tweets function
+  function loadTweets() {
+    $.ajax("/tweets")
+    .then(function(tweets) {
+      console.log(tweets);
+      renderTweets(tweets);
+    });
+  };
 
-  // Call render tweets function based on user database
-  renderTweets(data);
+  // Call AJAX to render tweets
+  $(".new-tweet form").submit(function(a) {
+    a.preventDefault();
+    $.ajax({
+      url: $(this).attr("action"),
+      method: 'POST',
+      data: $(this).serialize()
+    })
+      .then(function(tweets) {
+        loadTweets();
+        console.log("It works");
+    });
+  });
 
 });
